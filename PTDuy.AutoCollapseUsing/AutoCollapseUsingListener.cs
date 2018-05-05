62b5dc2c36e36eee7fd5943d0a0a88174edec531
Duy IT <duy.it@outlook.com>
2018-05-05 09:47:26 +0700

    Initial project

---
+++ PTDuy.AutoCollapseUsing/AutoCollapseUsingListener.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+
+namespace Duy.AutoCollapseUsing
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [ContentType("CSharp")]
+    public class AutoCollapseUsingListener : IWpfTextViewCreationListener
+    {
+        [Import]
+        internal IOutliningManagerService OutliningManagerService { get; set; }
+
+        private IWpfTextView _textView;
+        private bool _isEverCollapse;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            if (OutliningManagerService == null || textView == null)
+            {
+                return;
+            }
+
+            IOutliningManager outliningManager = OutliningManagerService.GetOutliningManager(textView);
+
+            if (outliningManager == null)
+            {
+                return;
+            }
+
+            outliningManager.RegionsChanged += OnRegionsChanged;
+            _textView = textView;
+            _isEverCollapse = false;
+        }
+
+        private void OnRegionsChanged(object sender, RegionsChangedEventArgs regionsChangedEventArgs)
+        {
+            var outliningManager = sender as IOutliningManager;
+            if (outliningManager != null && outliningManager.Enabled)
+            {
+                // Collapses all of the regions within the span where Match() returns true.
+                outliningManager.CollapseAll(regionsChangedEventArgs.AffectedSpan, Match);
+            }
+        }
+
+        // Returns true when the collapsible should be collapsed.
+        private bool Match(ICollapsible collapsible)
+        {
+            try
+            {
+                if (_isEverCollapse)
+                    return false;
+
+                var usingText = collapsible?.Extent?.GetText(_textView.TextSnapshot);
+
+                var isUsingRegion = !string.IsNullOrEmpty(usingText) && usingText.Contains("using ");
+
+                if (isUsingRegion)
+                {
+                    _isEverCollapse = true;
+
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                ActivityLog.LogError("AutoCollapseUsing", ex.Message);
+
+                return false;
+            }
+        }
+    }
+}
